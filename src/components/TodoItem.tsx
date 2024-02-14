5584df1f528c5cfdcd61dc5f0ed1a03353f0e834
summermong <2514o_o@naver.com>
2024-02-15 00:08:30 +0900

    :sparkles: FEAT: TODO 삭제 기능

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import Styles from './TodoItem.module.css';
 
-const TodoItem: React.FC<{ text: string }> = (props) => {
-  return <li className={Styles.item}>{props.text}</li>;
+const TodoItem: React.FC<{
+  text: string;
+  onRemoveTodo: (event: React.MouseEvent) => void;
+}> = (props) => {
+  return (
+    <li className={Styles.item}>
+      <div>{props.text}</div>
+      <button onClick={props.onRemoveTodo}>❌</button>
+    </li>
+  );
 };
 
 export default TodoItem;
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -3,11 +3,17 @@ import Todo from '../models/todo';
 import TodoItem from './TodoItem';
 import Styles from './Todos.module.css';
 
-const Todos: React.FC<{ items: Todo[] }> = (props) => {
+const Todos: React.FC<{ items: Todo[]; onRemoveTodo: (id: string) => void }> = (
+  props
+) => {
   return (
     <ul className={Styles.todos}>
       {props.items.map((item) => (
-        <TodoItem key={item.id} text={item.text} />
+        <TodoItem
+          key={item.id}
+          text={item.text}
+          onRemoveTodo={props.onRemoveTodo.bind(null, item.id)}
+        />
       ))}
     </ul>
   );
