b5a9c356e025a88235dd5f8f3e10f8bb80defa66
summermong <2514o_o@naver.com>
2024-02-14 23:43:31 +0900

    :lipstick: STYLE: CSS 적용

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
+import Styles from './TodoItem.module.css';
 
 const TodoItem: React.FC<{ text: string }> = (props) => {
-  return <li>{props.text}</li>;
+  return <li className={Styles.item}>{props.text}</li>;
 };
 
 export default TodoItem;
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Todo from '../models/todo';
 import TodoItem from './TodoItem';
+import Styles from './Todos.module.css';
 
 const Todos: React.FC<{ items: Todo[] }> = (props) => {
   return (
-    <ul>
+    <ul className={Styles.todos}>
       {props.items.map((item) => (
         <TodoItem key={item.id} text={item.text} />
       ))}
