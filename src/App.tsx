e04b4e52d47a897c5519a7c932af5fe46cfde74b
summermong <2514o_o@naver.com>
2024-02-14 22:46:06 +0900

    :sparkles: FEAT: 동적으로 TODO 추가하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
+import { useState } from 'react';
 import './App.css';
+import NewTodo from './components/NewTodo';
 import Todos from './components/Todos';
 import Todo from './models/todo';
 
 function App() {
-  const todos = [new Todo('Learn React'), new Todo('Learn Typescript')];
+  const [todos, setTodos] = useState<Todo[]>([]); // 처음엔 빈 배열이지만 나중에 배열에 항목을 추가할 떄는 Todo[] 타입이다.
+
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+    setTodos((prevTodos) => {
+      return prevTodos.concat(newTodo);
+    });
+  };
   return (
     <div>
+      <NewTodo onAddTodo={addTodoHandler} />
       <Todos items={todos} />
     </div>
   );
---
+++ src/components/NewTodo.tsx
@@ -0,0 +1,26 @@
+import { useRef } from 'react';
+
+const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
+  const todoTextInputRef = useRef<HTMLInputElement>(null);
+
+  const submitHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+
+    const enteredText = todoTextInputRef.current!.value; //!. (null이 아님을 확신할 때)
+
+    if (enteredText.trim().length === 0) {
+      return;
+    }
+
+    props.onAddTodo(enteredText);
+  };
+  return (
+    <form onSubmit={submitHandler}>
+      <label htmlFor="text">Todo Text</label>
+      <input type="text" id="text" ref={todoTextInputRef} />
+      <button>Add Todo</button>
+    </form>
+  );
+};
+
+export default NewTodo;
